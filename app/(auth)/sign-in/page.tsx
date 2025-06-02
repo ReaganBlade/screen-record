ed4cfc0bd914cc5c0f69a98ec74890a9893f4159
Rohit Tudu <rohittudu181@gmail.com>
2025-06-02 22:11:23 +0530

    Upload Implementation

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,17 @@
+'use client';
+
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { authClient } from "@/lib/auth-client";
 
 const page = () => {
+  const handleSignIn = async () => {
+    return await authClient.signIn.social({
+      provider: 'google'
+    });
+  }
+
   return (
     <main className="sign-in">
       <aside className="testimonial">
@@ -65,7 +74,7 @@ const page = () => {
             Create and share your very first <span>SnapCast video</span> in no
             time!
           </p>
-          <button>
+          <button onClick={handleSignIn}>
             <Image
               src="/assets/icons/google.svg"
               alt="google"
